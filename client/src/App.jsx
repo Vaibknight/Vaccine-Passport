daeccdf04defdd7f5a62f2ae0881f36d8ad0bed3
shaman <fayazhossain88@gmail.com>
2023-06-23 22:02:57 +0530

    completed project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Login, AppLayout, Dashboard, User, UserCreate, UserDetail, Vaccine, VaccineDetail, Place, PlaceDetail, QRScan } from './pages'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="login" element={<Login/>}/>
+                <Route path="" element={<AppLayout/>}>
+                    <Route index element={<Dashboard/>}/>
+                    <Route path='/user' element={<User/>}/>
+                    <Route path='/user/create' element={<UserCreate/>}/>
+                    <Route path='/user/:id' element={<UserDetail/>}/>
+                    <Route path='/vaccine' element={<Vaccine/>}/>
+                    <Route path='/vaccine/:id' element={<VaccineDetail/>}/>
+                    <Route path='/place' element={<Place/>}/>
+                    <Route path='/place/:id' element={<PlaceDetail/>}/>
+                    <Route path='/qr-scan' element={<QRScan/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import CssBaseline from "@mui/material/CssBaseline";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
-)
+  document.getElementById("root")
+);
